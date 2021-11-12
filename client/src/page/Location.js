808d8e9c67d2ce8f45e6553b0a204c04bae89deb
dewathiwat <Athiwat_Jansomwong@cmu.ac.th>
2021-11-12 17:00:59 +0700

    add map page

---
+++ client/src/page/Location.js
@@ -0,0 +1,60 @@
+import React from "react";
+import styled from "styled-components";
+import Header from "../components/Header";
+import { longdo, map, LongdoMap } from "../components/longdo-map/LongdoMap";
+
+function Location({ className, name, description, lat, lon }) {
+  function initMap() {
+    map.Layers.setBase(longdo.Layers.GRAY);
+    map.Ui.Crosshair.visible(false);
+    if(name)
+    {map.Overlays.add(
+      new longdo.Marker(
+        { lon: lon, lat: lat },
+        {
+          title: name,
+          detail: description ,
+        }
+      )
+    );
+    }  
+    
+  }
+  const mapKey = "5e3612dcbfa88a77bf9cc6773e5a1545";
+  return (
+    <div className={className}>
+      <Header />
+      <div className="map-box">
+        <LongdoMap id="longdo-map" mapKey={mapKey} callback={initMap} />
+      </div>
+      <div className="name-location">{name}</div>
+      <div className="description-box">
+        <div className="des-text">{description}</div>
+      </div>
+    </div>
+  );
+}
+
+export default styled(Location)`
+  .map-box {
+    padding: 30px 150px;
+  }
+  .name-location {
+    padding-left: 200px;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 36px;
+  }
+  .description-box {
+    margin: 30px 150px 30px 150px;
+    height: 300px;
+    background: white;
+  }
+  .des-text {
+    padding: 50px;
+    font-style: normal;
+    font-weight: 300;
+    font-size: 24px;
+    line-height: 28px;
+  }
+`;
